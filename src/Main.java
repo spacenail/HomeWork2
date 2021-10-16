9c83a4245e8e2b147c9ae89d9157e165361229cf
Artem Meshcherov <spacenail@gmail.com>
2021-10-17 00:32:20 +0300

    v.1

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,27 @@ public class Main {
          Найти и вывести список уникальных слов, из которых состоит массив (дубликаты не считаем).
          Посчитать сколько раз встречается каждое слово.
          */
-        List<String> list = new ArrayList<String>(Arrays.asList("Java", "Multithreading", "JDBC", "Servlet", "Swing",
+        List<String> list = new ArrayList<>(Arrays.asList("Java", "Multithreading", "JDBC", "Servlet", "Swing",
                 "Spring","Maven","SQL","Git", "Java", "Servlet", "HTML" ,"CSS"));
         System.out.println(list);
-        Set<String> set = new HashSet<String>(list);
+        Set<String> set = new HashSet<>(list);
         System.out.println(set);
         for (String s:set){
             System.out.println("В списке содержится " + Collections.frequency(list,s) + " " + s);
         }
+        /*
+        2. Написать простой класс ТелефонныйСправочник, который хранит в себе список фамилий и телефонных
+         номеров. В этот телефонный справочник с помощью метода add() можно добавлять записи.
+         С помощью метода get() искать номер телефона по фамилии. Следует учесть, что под одной
+         фамилией может быть несколько телефонов (в случае однофамильцев), тогда при запросе такой
+         фамилии должны выводиться все телефоны.
+         */
 
-
-
-
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Willis","01");
+        phoneBook.add("Willis","01");
+        phoneBook.add("Willis","89294442323");
+        phoneBook.get("Willis");
+        phoneBook.get("Payne");
     }
 }
--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class PhoneBook {
           /*
         2. Написать простой класс ТелефонныйСправочник, который хранит в себе список фамилий и телефонных
@@ -6,4 +8,23 @@ public class PhoneBook {
          фамилией может быть несколько телефонов (в случае однофамильцев), тогда при запросе такой
          фамилии должны выводиться все телефоны.
          */
+   private final Map<String, Set<String> > phoneBook = new HashMap<>();
+
+    public void add(String lastName, String phoneNumber){
+        if(phoneBook.containsKey(lastName)) {
+            phoneBook.get(lastName).add(phoneNumber);
+        }else{
+            Set<String> set = new HashSet<>();
+            set.add(phoneNumber);
+            phoneBook.put(lastName,set);
+        }
+    }
+
+    public void get(String lastName){
+        try {
+            System.out.println(lastName + " " + phoneBook.get(lastName).toString());
+        } catch (NullPointerException e){
+            System.out.println("Фамилия " + lastName + " не найдена!");
+        }
+    }
 }
