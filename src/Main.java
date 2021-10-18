f0283f03f9a257f246002a31a3e6744add1021bf
Artem Meshcherov <spacenail@gmail.com>
2021-10-18 19:01:44 +0300

    v.2 change sumIntArray check size

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,19 @@ public class Main {
     public static void main(String[] args) {
         String[][] wrongArray = {{"1", "2", "3"}, {"a", "b", "c"}};
         String[][] partlyCorrectArray = {{"1", "2", "3", "4"}, {"a", "b", "c", "d"}, {"e", "f", "g", "h"}, {"i", "j", "k", "l"}};
-        String[][] correctArray = {{"1", "2", "3", "4"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}};
+        String[][] correctArray = {{"1", "2", "3", "4"}, {"5", "6", "7","8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}};
         System.out.println(sumIntArray(correctArray));
     }
 
-
     static public int sumIntArray(String[][] stringArray) {
-        if (stringArray.length != 4 && stringArray[0].length != 4) {
+        if (stringArray.length != 4) {
             throw new MyArraySizeException();
+        }
+
+        for (int i = 0; i < stringArray.length; i++) {
+            if(stringArray[i].length != 4){
+                throw new MyArraySizeException();
+            }
         }
         return adder(stringArray);
     }
