8f124cbf2c751b9538f6ba23d394831cc83a9d3e
Artem Meshcherov <spacenail@gmail.com>
2021-10-26 00:04:44 +0300

    v2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,74 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.Collections;
 
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+/*
+1. Необходимо написать два метода, которые делают следующее:
+1) Создают одномерный длинный массив, например:
+static final int size = 10000000;
+static final int h = size / 2;
+float[] arr = new float[size];
+2) Заполняют этот массив единицами;
+3) Засекают время выполнения: long a = System.currentTimeMillis();
+4) Проходят по всему массиву и для каждой ячейки считают новое значение по формуле:
+arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+5) Проверяется время окончания метода System.currentTimeMillis();
+6) В консоль выводится время работы: System.out.println(System.currentTimeMillis() - a);
+
+Отличие первого метода от второго:
+Первый просто бежит по массиву и вычисляет значения.
+Второй разбивает массив на два массива, в двух потоках высчитывает новые значения и потом склеивает эти массивы обратно в один.
+ */
+        singleThreadCalcArray();
+        multiThreadCalcArray();
+    }
+
+    public static void singleThreadCalcArray(){
+        final int size = 10000000;
+        float[] arr = new float[size];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1;
+        }
+
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+
+        System.out.println(System.currentTimeMillis() - a);
+    }
+
+    public static void multiThreadCalcArray() throws InterruptedException {
+        final int size = 10000000;
+        final int h = size / 2;
+        float[] arr = new float[size];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1;
+        }
+
+        long a = System.currentTimeMillis();
+
+        float[] a1 = arr.clone();
+        float[] a2 = arr.clone();
+
+        Thread t0 = new Thread(()-> {
+            for (int i = h; i < a2.length; i++) {
+                a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+
+        t0.start();
+
+        for (int i = 0; i < h; i++) {
+            a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+
+        System.arraycopy(a1, 0, arr, 0, h);
+        t0.join();
+        System.arraycopy(a2, h, arr, h, h);
+        System.out.println(System.currentTimeMillis() - a);
     }
 }
